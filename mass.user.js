ad66fdc9d985e2145c74c6e590d6fdaf62e6e16a
secretsystem <ian.april28@gmail.com>
2015-09-05 20:06:21 +0700

    Update mass.user.js

--- mass.user.js
+++ mass.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         agar-mass-ejector
-// @namespace    http://github.com/dimotsai/
+// @namespace    http://agar.io/
 // @version      0.03
 // @description  A faster, continuous mass ejector for agar.
 // @author       dimotsai
 // @license      MIT
-// @match        http://agar.io/*
+// @match        http://agar.io/
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
